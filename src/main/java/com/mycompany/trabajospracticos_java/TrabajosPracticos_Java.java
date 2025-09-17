a2ad0e5894e1e7805460ed758c6b088a10cf829e
Augusto <Augusto@DESKTOP-S338UT1>
2025-09-17 12:31:30 -0300

    Tp02

---
+++ src/main/java/com/mycompany/trabajospracticos_java/TrabajosPracticos_Java.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabajospracticos_java;
+
+/**
+ *
+ * @author Augusto
+ */
+public class TrabajosPracticos_Java {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
